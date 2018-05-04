52ce95a8a5526c3c7e5bef8e381afc91a2855dbb
pathmes <pathmes@gmail.com>
2018-05-04 23:26:31 +0530

    Demo version of departmental h-index for the past 5 years

--- h2013-generic.R
+++ h2013-generic.R
@@ -29,9 +29,32 @@ for (i in 1:n) {
     df[j, 9] <-  get(paste("df", i, j, sep = "")) %>%
       filter(year > 2012) %>%
       summarize(sum(cites))
-#     rm(list = (paste("df", i, j, sep = "")))
+    
+    rm(list = (paste("df", i, j, sep = "")))
   }
   
 assign(paste("b", i, sep = ""), df)
 }
+
+rm(df)
+
+# Make a list of data frames
+df_list <- mget(ls(pattern = "b[0-9]"))
+
+# Make a dataframe for the department combining the individual dataframes
+dept <- bind_rows(df_list)
+
+# Drop duplicate publications resulting from papers authored by 
+# more than one academic from the department
+dept_u <- distinct(dept, cid, .keep_all = TRUE)
+
+# Rank the combined list of publications by the number of citations from 2013
+dept_u$crank <- row_number(-dept_u$c2013)
+
+# Sort the dataframe based on the citations from 2013
+dept_u <- dept_u[order(-dept_u$c2013),]
+
+# Identify the h-index for the department.
+
+dept_h_index <- max(dept_u$crank[which(dept_u$c2013 >= dept_u$crank)]); dept_h_index
 
