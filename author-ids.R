c1249d0c0d9bbe6f2f9108c13e3a8ec303321fea
pathmes <pathmes@gmail.com>
2018-05-05 13:51:28 +0530

    Author ids by department - med & ph only - to be completed

---
+++ author-ids.R
@@ -0,0 +1,7 @@
+# Google scholar author ids - Dept of Public Health
+id <- c("mc3dJxsAAAAJ", "ASvjn0IAAAAJ", "tfhM6eQAAAAJ", "D7uvADUAAAAJ", 
+        "3D2VR5QAAAAJ", "wv1o_1IAAAAJ", "QD_s8XwAAAAJ")
+
+# Google scholar author ids - Dept of Medicine
+id <- c("YzxxFJwAAAAJ", "pZ28YVgAAAAJ", "bXoIKFwAAAAJ", "fxUToyAAAAAJ", 
+        "gDi6RrEAAAAJ", "BYZ-R9AAAAAJ", "QTmUa8EAAAAJ", "jp7ngMMAAAAJ")
