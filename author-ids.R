4cd71ea995cc30f330e30f05785b08c28db36049
pathmes <pathmes@gmail.com>
2018-05-06 22:31:00 +0530

    Added loading of packages - scholar & tidyverse. Added the line source("h2013-generic.R") after every id assign statement.

--- author-ids.R
+++ author-ids.R
@@ -1,10 +1,23 @@
+# Load the required packages.  Will install the package, 
+# if not already installed.
+if (!require(scholar))
+  install.packages('scholar')
+library(scholar)
+if (!require(tidyverse))
+  install.packages('tidyverse')
+library(tidyverse)
+
+
 # Author ids of BK & LA to see whether h2013-generic.R is working ok
 id <- c("3D2VR5QAAAAJ", "wv1o_1IAAAAJ")
+source("h2013-generic.R")
 
 # Google scholar author ids - Dept of Public Health
 id <- c("mc3dJxsAAAAJ", "ASvjn0IAAAAJ", "tfhM6eQAAAAJ", "D7uvADUAAAAJ", 
         "3D2VR5QAAAAJ", "wv1o_1IAAAAJ", "QD_s8XwAAAAJ")
+source("h2013-generic.R")
 
 # Google scholar author ids - Dept of Medicine
 id <- c("YzxxFJwAAAAJ", "pZ28YVgAAAAJ", "bXoIKFwAAAAJ", "fxUToyAAAAAJ", 
         "gDi6RrEAAAAJ", "BYZ-R9AAAAAJ", "QTmUa8EAAAAJ", "jp7ngMMAAAAJ")
+source("h2013-generic.R")
