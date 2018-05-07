044585abd1cba245b21a9b13089c70d285232eb2
pathmes <pathmes@gmail.com>
2018-05-07 06:19:34 +0530

    Added a command to clear workspace at the top of each chunk

--- author-ids.R
+++ author-ids.R
@@ -7,16 +7,18 @@ if (!require(tidyverse))
   install.packages('tidyverse')
 library(tidyverse)
 
-
+rm(list = ls())
 # Author ids of BK & LA to see whether h2013-generic.R is working ok
 id <- c("3D2VR5QAAAAJ", "wv1o_1IAAAAJ")
 source("h2013-generic.R")
 
+rm(list = ls())
 # Google scholar author ids - Dept of Public Health
 id <- c("mc3dJxsAAAAJ", "ASvjn0IAAAAJ", "tfhM6eQAAAAJ", "D7uvADUAAAAJ", 
         "3D2VR5QAAAAJ", "wv1o_1IAAAAJ", "QD_s8XwAAAAJ")
 source("h2013-generic.R")
 
+rm(list = ls())
 # Google scholar author ids - Dept of Medicine
 id <- c("YzxxFJwAAAAJ", "pZ28YVgAAAAJ", "bXoIKFwAAAAJ", "fxUToyAAAAAJ", 
         "gDi6RrEAAAAJ", "BYZ-R9AAAAAJ", "QTmUa8EAAAAJ", "jp7ngMMAAAAJ")
